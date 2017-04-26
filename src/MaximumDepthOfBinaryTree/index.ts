f623722f7ae3c29285e79a69c775a15365ee1f39
Hanruis <183631698@qq.com>
2017-04-26 12:07:02 +0800

    微调

--- src/MaximumDepthOfBinaryTree/index.ts
+++ src/MaximumDepthOfBinaryTree/index.ts
@@ -9,16 +9,16 @@
  * @param {TreeNode} root
  * @return {number}
  */
-interface TreeNode{
+export interface TreeNode {
     val: number;
     left: TreeNode|null;
     right: TreeNode|null;
-} 
+}
 
 function findMaxDepth(node: TreeNode, depth: number): number {
-    if (!node){
+    if (!node) {
         return depth;
-    }else{
+    } else {
         depth += 1;
         return Math.max(findMaxDepth(node.left, depth), findMaxDepth(node.right, depth));
     }
@@ -29,4 +29,6 @@ function maxDepth(root: TreeNode): number {
     return findMaxDepth(root, 0);
 }
 
-export = maxDepth;
+export {
+    maxDepth
+};
---
+++ src/SingleNumber/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+declare const singleNumber: (nums: number[]) => number;
+export = singleNumber;
